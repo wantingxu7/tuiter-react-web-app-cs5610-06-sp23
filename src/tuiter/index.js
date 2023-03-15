bbf3cf2ab7d15aba77066be9216cd1a9c771c3f7
wantingxu <xu.wan@northeastern.edu>
2023-03-14 17:12:34 -0700

    finish 3.2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,6 +1,6 @@
 import Nav from "../nav";
 import NavigationSidebar
-    from "./navigation-sidebar";
+  from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list/index.js";
 import PostSummaryItem from "./post-summary-list/post-summary-item.js";
 import PostSummaryList from "./post-summary-list/index";
@@ -8,33 +8,41 @@ import ExploreComponent from "./explore/index";
 import HomeComponent from "./home/index";
 import {Routes, Route} from "react-router";
 import React from "react";
+import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
 
+
+const store = configureStore({reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
-    return (
-        <div className="row mt-2">
-            <div className="col-2 col-xs-2 col-sm-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="explore"/>
-            </div>
-            <div className="col-10 col-xs-10 col-sm-10 col-md-10 col-lg-7 col-xl-6"
-                 style={{"position": "relative"}}>
-
-                <Routes>
-                    <Route path="home" element={<HomeComponent/>}/>
-                    <Route path="/" element={<ExploreComponent/>}/>
-                    <Route path="explore" element={<ExploreComponent/>}/>
-                </Routes>
-
-            </div>
-            <div className="col-xl-4 col-lg-4 d-none d-lg-block">
-                <Routes>
-                    <Route path="home" element={<WhoToFollowList/>}/>
-                    <Route path="/" element={<WhoToFollowList/>}/>
-                    <Route path="explore" element={<WhoToFollowList/>}/>
-                </Routes>
-            </div>
+  return (
+    <Provider store={store}>
+      <div className="row mt-2">
+        <div className="col-2 col-xs-2 col-sm-2 col-md-2 col-lg-1 col-xl-2">
+          <NavigationSidebar active="explore"/>
         </div>
-    )
+        <div className="col-10 col-xs-10 col-sm-10 col-md-10 col-lg-7 col-xl-6"
+             style={{"position": "relative"}}>
+
+          <Routes>
+            <Route path="home" element={<HomeComponent/>}/>
+            <Route path="/" element={<ExploreComponent/>}/>
+            <Route path="explore" element={<ExploreComponent/>}/>
+          </Routes>
+
+        </div>
+        <div className="col-xl-4 col-lg-4 d-none d-lg-block">
+          <Routes>
+            <Route path="home" element={<WhoToFollowList/>}/>
+            <Route path="/" element={<WhoToFollowList/>}/>
+            <Route path="explore" element={<WhoToFollowList/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Provider>
+  )
 }
 
 export default Tuiter
--- src/tuiter/who-to-follow-list/index.js
+++ src/tuiter/who-to-follow-list/index.js
@@ -1,23 +1,25 @@
 import React from "react";
-import whoArray from './who.json';
-import WhoToFollowListItem
-    from "./who-to-follow-list-item";
+// import whoArray from '../data/who.json';
+import WhoToFollowListItem from "./who-to-follow-list-item";
+import {useSelector} from "react-redux";
 
 const WhoToFollowList = () => {
-    return (
-        <ul className="list-group wd-font-18">
-            <li className="list-group-item">
-                <h3 className="fw-bold m-0" style={{fontSize: "20px"}}>Who to follow</h3>
-            </li>
-            {
-                whoArray.map(who =>
-                    <WhoToFollowListItem
-                        key={who._id}
-                        who={who}/>
-                )
-            }
-        </ul>
-    );
+  const whoArray = useSelector((state) => state.who);
+
+  return (
+    <ul className="list-group wd-font-18">
+      <li className="list-group-item">
+        <h3 className="fw-bold m-0" style={{fontSize: "20px"}}>Who to follow</h3>
+      </li>
+      {
+        whoArray.map(who =>
+          <WhoToFollowListItem
+            key={who._id}
+            who={who}/>
+        )
+      }
+    </ul>
+  );
 };
 
 export default WhoToFollowList;
